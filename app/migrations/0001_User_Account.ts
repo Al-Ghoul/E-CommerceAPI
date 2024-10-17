7b9e35b6a3cdf0428c2a8b94871840a9d7b61266
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-17 14:08:12 +0300

    feat: add user/account's initial migration

---
+++ app/migrations/0001_User_Account.ts
@@ -0,0 +1,44 @@
+import { Kysely, sql } from "kysely";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable("user")
+    .addColumn("id", "integer", (col) => col.primaryKey())
+    .addColumn("email", "text", (col) => col.unique())
+    .addColumn("first_name", "text")
+    .addColumn("last_name", "text")
+    .addColumn("emailVerified", "timestamptz")
+    .addColumn("image", "text")
+    .execute();
+
+  await db.schema
+    .createTable("account")
+    .addColumn("id", "integer", (col) => col.primaryKey())
+    .addColumn("password", "varchar")
+    .addColumn("userId", "bigint", (col) =>
+      col.references("user.id").onDelete("cascade").notNull(),
+    )
+    .addColumn("type", "text", (col) => col.notNull())
+    .addColumn("provider", "text", (col) => col.notNull())
+    .addColumn("providerAccountId", "text", (col) => col.notNull())
+    .addColumn("refresh_token", "text")
+    .addColumn("access_token", "text")
+    .addColumn("id_token", "text")
+    .addColumn("expires_at", "bigint")
+    .addColumn("scope", "text")
+    .addColumn("created_at", "timestamp", (col) =>
+      col.defaultTo(sql`CURRENT_TIMESTAMP`).notNull(),
+    )
+    .execute();
+
+  await db.schema
+    .createIndex("Account_userId_index")
+    .on("account")
+    .column("userId")
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable("account").execute();
+  await db.schema.dropTable("user").execute();
+}
