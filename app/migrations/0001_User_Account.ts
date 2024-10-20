d0fffaaa495e8903cd801d418ec3d45a077b54ed
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-20 08:27:05 +0300

    feat: add entity relationship diagram

--- app/migrations/0001_User_Account.ts
+++ app/migrations/0001_User_Account.ts
@@ -4,11 +4,11 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("user")
     .addColumn("id", "bigserial", (col) => col.primaryKey())
-    .addColumn("email", "text", (col) => col.unique())
-    .addColumn("first_name", "text")
-    .addColumn("last_name", "text")
-    .addColumn("emailVerified", "timestamptz")
-    .addColumn("image", "text")
+    .addColumn("email", "varchar(50)", (col) => col.unique())
+    .addColumn("first_name", "varchar(50)")
+    .addColumn("last_name", "varchar(50)")
+    .addColumn("emailVerified", "timestamp")
+    .addColumn("image", "varchar(255)")
     .addColumn("created_at", "timestamp", (col) =>
       col.defaultTo(sql`now()`).notNull(),
     )
@@ -20,18 +20,18 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable("account")
     .addColumn("id", "bigserial", (col) => col.primaryKey())
-    .addColumn("password", "varchar")
-    .addColumn("userId", "bigint", (col) =>
+    .addColumn("password", "varchar(255)")
+    .addColumn("user_id", "bigint", (col) =>
       col.references("user.id").onDelete("cascade").notNull(),
     )
-    .addColumn("type", "text", (col) => col.notNull())
-    .addColumn("provider", "text", (col) => col.notNull())
-    .addColumn("providerAccountId", "text", (col) => col.notNull())
+    .addColumn("type", "varchar(50)", (col) => col.notNull())
+    .addColumn("provider", "varchar(50)", (col) => col.notNull())
+    .addColumn("providerAccount_id", "varchar(50)", (col) => col.notNull())
     .addColumn("refresh_token", "text")
     .addColumn("access_token", "text")
     .addColumn("id_token", "text")
     .addColumn("expires_in", "bigint")
-    .addColumn("scope", "text")
+    .addColumn("scope", "varchar(255)")
     .addColumn("created_at", "timestamp", (col) =>
       col.defaultTo(sql`now()`).notNull(),
     )
@@ -41,13 +41,14 @@ export async function up(db: Kysely<any>): Promise<void> {
     .execute();
 
   await db.schema
-    .createIndex("Account_userId_index")
+    .createIndex("Account_user_id_index")
     .on("account")
-    .column("userId")
+    .column("user_id")
     .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropIndex("Account_user_id_index").execute();
   await db.schema.dropTable("account").execute();
   await db.schema.dropTable("user").execute();
 }
