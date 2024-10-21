e02517b2294f60fc2b7fe591d7cdd718f8b0b820
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-21 18:46:23 +0300

    feat(Cart): add carts RESTful API

---
+++ app/src/app/api/users/[id]/cart/route.ts
@@ -0,0 +1,188 @@
+import { db } from "@/db";
+import * as jose from "jose";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const accecssToken = req.headers.get("authorization")?.split(" ")[1];
+
+  try {
+    const tokenData = await jose.jwtVerify(
+      accecssToken!,
+      new TextEncoder().encode(process.env.TOKEN_SECRET),
+      {
+        typ: "access",
+        issuer: process.env.TOKEN_ISSUER,
+        audience: process.env.TOKEN_ISSUER,
+      },
+    );
+
+    const user_id = tokenData.payload.sub?.split("|")[1]!;
+    if (params.id != user_id) {
+      return new Response(
+        JSON.stringify({
+          status: "error",
+          statusCode: 401,
+          message: "Unauthorized access.",
+        }),
+        {
+          status: 401,
+        },
+      );
+    }
+
+    const getCart = await db
+      .selectFrom("cart")
+      .select(["id"])
+      .where("user_id", "=", user_id)
+      .where("status", "=", "active")
+      .executeTakeFirst();
+
+    if (getCart) {
+      return new Response(
+        JSON.stringify({
+          status: "error",
+          statusCode: 409,
+          message: "Cart already exists.",
+        }),
+        {
+          status: 409,
+        },
+      );
+    }
+
+    const result = await db
+      .insertInto("cart")
+      .values({
+        user_id: user_id,
+        status: "active",
+      })
+      .returningAll()
+      .executeTakeFirst();
+
+    return new Response(
+      JSON.stringify({
+        data: result,
+        status: "success",
+        message: "Cart was created successfully!",
+        statusCode: 201,
+      }),
+      {
+        status: 201,
+      },
+    );
+  } catch (err) {
+    if (err instanceof jose.errors.JOSEError) {
+      return new Response(
+        JSON.stringify({
+          status: "error",
+          statusCode: 401,
+          message: "Invalid or expired access token",
+          detail: "Please re-login",
+        }),
+        { status: 401 },
+      );
+    }
+
+    return new Response(
+      JSON.stringify({
+        status: "error",
+        statusCode: 500,
+        message: "Something went wrong. Please try again later.",
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const accecssToken = req.headers.get("authorization")?.split(" ")[1];
+
+  try {
+    const tokenData = await jose.jwtVerify(
+      accecssToken!,
+      new TextEncoder().encode(process.env.TOKEN_SECRET),
+      {
+        typ: "access",
+        issuer: process.env.TOKEN_ISSUER,
+        audience: process.env.TOKEN_ISSUER,
+      },
+    );
+
+    const user_id = tokenData.payload.sub?.split("|")[1]!;
+    if (params.id != user_id) {
+      return new Response(
+        JSON.stringify({
+          status: "error",
+          statusCode: 401,
+          message: "Unauthorized access.",
+        }),
+        {
+          status: 401,
+        },
+      );
+    }
+
+    const cart = await db
+      .selectFrom("cart")
+      .selectAll()
+      .where("user_id", "=", params.id)
+      .where("status", "=", "active")
+      .executeTakeFirst();
+
+    if (!cart) {
+      return new Response(
+        JSON.stringify({
+          status: "error",
+          statusCode: 404,
+          message: "Cart not found.",
+          detail: "There's no active cart, please create one.",
+        }),
+        {
+          status: 404,
+        },
+      );
+    }
+
+    return new Response(
+      JSON.stringify({
+        data: cart,
+        status: "success",
+        message: "Cart was fetched successfully!",
+        statusCode: 200,
+      }),
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    if (err instanceof jose.errors.JOSEError) {
+      return new Response(
+        JSON.stringify({
+          status: "error",
+          statusCode: 401,
+          message: "Invalid or expired access token",
+          detail: "Please re-login",
+        }),
+        { status: 401 },
+      );
+    }
+
+    return new Response(
+      JSON.stringify({
+        status: "error",
+        statusCode: 500,
+        message: "Something went wrong. Please try again later.",
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+}
