734133bcaa89eac0493a6818853026bb5d6e0075
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-26 12:31:14 +0300

    refactor: move the modal up the heirarchy

--- app/src/app/QueryClientProvider.tsx
+++ app/src/app/QueryClientProvider.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { AuthContext } from "@/lib/contexts";
 import { QueryClient, QueryClientProvider as QCP } from "@tanstack/react-query";
+import { Header } from "@/components/ui/core/header";
+import { SignUpModal } from "@/components/ui/modal";
+import { useState } from "react";
+
+const queryClient = new QueryClient();
 
 export default function QueryClientProvider({
   children,
@@ -9,10 +14,13 @@ export default function QueryClientProvider({
   children: React.ReactNode;
   isLoggedIn: boolean;
 }>) {
-  const queryClient = new QueryClient();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <QCP client={queryClient}>
+      <Header onSignUpClick={() => setIsModalOpen(true)} />
       <AuthContext.Provider value={{ isAuthenticated: isLoggedIn }}>
+        <SignUpModal isOpen={isModalOpen} setIsOpenFN={setIsModalOpen} />
         {children}
       </AuthContext.Provider>
     </QCP>
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,58 +1,45 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Featured } from "@/components/ui/core/featured";
-import { Footer } from "@/components/ui/core/footer";
-import { Header } from "@/components/ui/core/header";
-import { SignUpModal } from "@/components/ui/modal";
 import { motion } from "framer-motion";
-import { useState } from "react";
-import { Toaster } from "react-hot-toast";
 
 export default function Home() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   return (
-    <div className="flex flex-col min-h-screen">
-      <SignUpModal isOpen={isModalOpen} setIsOpenFN={setIsModalOpen} />
-      <Header onSignUpClick={() => setIsModalOpen(true)} />
-      <motion.main
-        className="flex-1"
-        initial={{
-          opacity: 0,
-          y: 50,
-        }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          delay: 0,
-          duration: 0.7,
-        }}
-      >
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
-          <div className="px-4 md:px-6">
-            <div className="flex flex-col items-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Welcome to our E-Commerce Store
-                </h1>
-                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  Discover our wide range of products. From electronics to
-                  fashion, we&apos;ve got you covered.
-                </p>
-              </div>
-              <div className="space-x-4">
-                <Button>Shop Now</Button>
-                <Button variant="outline">Learn More</Button>
-              </div>
+    <motion.div
+      className="flex flex-col min-h-screen"
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      animate={{
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{
+        delay: 0,
+        duration: 0.7,
+      }}
+    >
+      <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <div className="px-4 md:px-6">
+          <div className="flex flex-col items-center space-y-4 text-center">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+                Welcome to our E-Commerce Store
+              </h1>
+              <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+                Discover our wide range of products. From electronics to
+                fashion, we&apos;ve got you covered.
+              </p>
+            </div>
+            <div className="space-x-4">
+              <Button>Shop Now</Button>
+              <Button variant="outline">Learn More</Button>
             </div>
           </div>
-        </section>
-        <Featured />
-      </motion.main>
-      <Toaster />
-      <Footer />
-    </div>
+        </div>
+      </section>
+      <Featured />
+    </motion.div>
   );
 }
