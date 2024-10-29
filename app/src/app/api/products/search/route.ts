9822086b2fbe30bb6cc2512774e75e784feef537
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-29 20:49:24 +0300

    feat: add products search RESTful endpoint

---
+++ app/src/app/api/products/search/route.ts
@@ -0,0 +1,41 @@
+import { db } from "@/db";
+import { sql, SqlBool } from "kysely";
+import { type NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const query = searchParams.get("q") + ":*";
+
+  try {
+    const products = await db
+      .selectFrom("product")
+      .selectAll()
+      .limit(10)
+      .where(
+        sql<SqlBool>`to_tsvector('simple', name) || to_tsvector('simple', description) @@ to_tsquery('simple', ${query})`,
+      )
+      .execute();
+
+    return new Response(
+      JSON.stringify({
+        data: products,
+        status: "success",
+        statusCode: 200,
+      }),
+      {
+        status: 200,
+      },
+    );
+  } catch {
+    return new Response(
+      JSON.stringify({
+        status: "error",
+        statusCode: 500,
+        message: "Couldn't parse your request, please try again later!",
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+}
