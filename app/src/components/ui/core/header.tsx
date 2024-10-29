04179cfba37deea39b55e2fc6a5dce1356b6eb19
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-10-29 15:08:57 +0300

    feat: add home & products links

--- app/src/components/ui/core/header.tsx
+++ app/src/components/ui/core/header.tsx
@@ -33,7 +33,13 @@ export function Header({ onSignUpClick }: { onSignUpClick: () => void }) {
       <nav className="ml-auto flex gap-4 sm:gap-6">
         <Link
           className="text-sm font-medium hover:underline underline-offset-4"
-          href="#"
+          href="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="text-sm font-medium hover:underline underline-offset-4"
+          href="/products"
         >
           Products
         </Link>
