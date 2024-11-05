1301c9fb6181bcaf775f12567b9f9d1784dd48d1
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-11-05 03:49:06 +0200

    feat(Nix): add std

---
+++ nix/repo/shells.nix
@@ -0,0 +1,23 @@
+/*
+This file holds reproducible shells with commands in them.
+
+They conveniently also generate config files in their startup hook.
+*/
+{
+  inputs,
+  cell,
+}: let
+  inherit (inputs.std) lib std;
+in
+  builtins.mapAttrs (_: lib.dev.mkShell) {
+    # Tool Homepage: https://numtide.github.io/devshell/
+    default = {
+      name = "E-CommerceAPI DevShell";
+
+      imports = [std.devshellProfiles.default];
+
+      commands = [
+        {package = inputs.nixpkgs.nodejs;}
+      ];
+    };
+  }
