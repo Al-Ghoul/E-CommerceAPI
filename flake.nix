9d606f9502350bdf491679f20d4f98bb21aa5cec
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-11-05 03:49:06 +0200

    feat(Nix): add std

--- flake.nix
+++ flake.nix
@@ -9,6 +9,14 @@
       url = "github:nix-community/NixNG/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    std = {
+      url = "github:divnix/std";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        devshell.url = "github:numtide/devshell";
+      };
+    };
   };
 
   nixConfig = {
@@ -16,72 +24,14 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = {
-    devshell,
-    nixpkgs,
-    nixng,
-    ...
-  }: let
-    system = "x86_64-linux";
-  in {
-    pkgs = nixpkgs.legacyPackages.${system};
-
-    devShells.${system}.default = let
-      pkgs = import nixpkgs {
-        inherit system;
-
-        overlays = [devshell.overlays.default];
-      };
-    in
-      pkgs.devshell.mkShell {
-        imports = [(pkgs.devshell.importTOML ./devshell.toml)];
-      };
-
-    db-image = let
-      nglib = nixng.nglib;
-    in
-      nglib.makeSystem {
-        inherit nixpkgs;
-        inherit system;
-        name = "e-commerce-postgres";
-        config = {pkgs, ...}: {
-          config = {
-            dumb-init = {
-              enable = true;
-              type.services = {};
-            };
-            nix = {
-              loadNixDb = true;
-              persistNix = "/nix-persist";
-              config = {
-                experimental-features = ["nix-command" "flakes"];
-                sandbox = true;
-                trusted-public-keys = [
-                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                ];
-                substituters = ["https://cache.nixos.org/"];
-              };
-            };
-            services.postgresql = {
-              enable = true;
-              package = pkgs.postgresql;
-              enableTCPIP = true;
-              initdbArgs = ["--encoding=UTF-8" "--locale=C"];
-              port = 5433;
-              initialScript = pkgs.writeText "db-initScript" ''
-                CREATE USER db_owner WITH LOGIN PASSWORD 'secure_password';
-                GRANT ALL PRIVILEGES ON DATABASE commercedb TO db_owner;
-
-                \c commercedb;
-                GRANT ALL ON SCHEMA public TO db_owner;
-              '';
-              ensureDatabases = ["commercedb"];
-              authentication = ''
-                host    all             db_owner             0.0.0.0/0            md5
-              '';
-            };
-          };
-        };
-      };
-  };
+  outputs = {std, ...} @ inputs:
+    std.growOn {
+      inherit inputs;
+      cellsFrom = ./nix;
+      cellBlocks = with std.blockTypes; [
+        (devshells "shells")
+      ];
+    } {
+      devShells = std.harvest inputs.self ["repo" "shells"];
+    };
 }
